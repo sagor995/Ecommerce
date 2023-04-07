da4fea798314a0d2a3efeb3fe5757c56d1aa9831
sagor995 <sagorahamed995@gmail.com>
2023-04-08 01:23:51 +0600

    adding demo menus in menubar1

--- resources/views/backend/includes/menubar.blade.php
+++ resources/views/backend/includes/menubar.blade.php
@@ -2,20 +2,20 @@
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
-            <img src="{{ asset('backend/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+            <img src="{{ asset('backend/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon" />
         </div>
         <div>
             <h4 class="logo-text">eCommerce Platform</h4>
         </div>
-        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+        <div class="toggle-icon ms-auto">
+            <i class="bx bx-arrow-to-left"></i>
         </div>
     </div>
     <!--navigation-->
     <ul class="metismenu" id="menu">
         <li>
             <a href="{{ route('admin.dashboard') }}" class="">
-                <div class="parent-icon"><i class='bx bx-home-circle'></i>
-                </div>
+                <div class="parent-icon"><i class="bx bx-home-circle"></i></div>
                 <div class="menu-title">Dashboard</div>
             </a>
         </li>
@@ -24,14 +24,19 @@
         <li class="menu-label">Product Management</li>
         <li>
             <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-category"></i>
-                </div>
+                <div class="parent-icon"><i class="bx bx-category"></i></div>
                 <div class="menu-title">Category</div>
             </a>
             <ul>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Add New Category</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Add New Category
+                    </a>
                 </li>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Manage All Category</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Manage All Category
+                    </a>
                 </li>
             </ul>
         </li>
@@ -40,12 +45,14 @@
         <li class="menu-label">Order Management</li>
         <li>
             <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-cart-alt"></i>
-                </div>
+                <div class="parent-icon"><i class="bx bx-cart-alt"></i></div>
                 <div class="menu-title">Orders</div>
             </a>
             <ul>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Manage All Order</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Manage All Order
+                    </a>
                 </li>
             </ul>
         </li>
@@ -55,14 +62,19 @@
         <li class="menu-label">Customer Management</li>
         <li>
             <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-group"></i>
-                </div>
+                <div class="parent-icon"><i class="bx bx-group"></i></div>
                 <div class="menu-title">Customer</div>
             </a>
             <ul>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Add New Customer</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Add New Customer
+                    </a>
                 </li>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Manage All Customer</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Manage All Customer
+                    </a>
                 </li>
             </ul>
         </li>
@@ -71,16 +83,24 @@
         <li class="menu-label">Platform Settings</li>
         <li>
             <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-wrench"></i>
-                </div>
+                <div class="parent-icon"><i class="bx bx-wrench"></i></div>
                 <div class="menu-title">Settings</div>
             </a>
             <ul>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Settings One</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Settings One
+                    </a>
                 </li>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Settings Two</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Settings Two
+                    </a>
                 </li>
-                <li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Settings Three</a>
+                <li>
+                    <a href="javascript:;">
+                        <i class="bx bx-right-arrow-alt"></i>Settings Three
+                    </a>
                 </li>
             </ul>
         </li>
